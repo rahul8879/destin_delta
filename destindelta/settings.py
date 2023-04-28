031f584790bfb60d956c2f63b276ba2d9281a9b3
unknown <rtiwarirahul123@gmail.com>
2023-04-28 11:00:07 +0530

    added few lines

--- destindelta/settings.py
+++ destindelta/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'bi_2pl0q*i4ebgfy_7lqczrqj62sl_f3mmb*pt=_m-_k+ze1tl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -166,11 +166,11 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Amazon S3 Configuration
 
-AWS_ACCESS_KEY_ID     = "AKIASRSHIPOG7KELAAGQ"
-AWS_SECRET_ACCESS_KEY ="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"
-AWS_STORAGE_BUCKET_NAME="destindelta-bucket-static"
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE ='storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_FILE_OVERWRITE = False
+# AWS_ACCESS_KEY_ID     = "AKIASRSHIPOG7KELAAGQ"
+# AWS_SECRET_ACCESS_KEY ="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"
+# AWS_STORAGE_BUCKET_NAME="destindelta-bucket-static"
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE ='storages.backends.s3boto3.S3Boto3Storage'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_FILE_OVERWRITE = False
 
